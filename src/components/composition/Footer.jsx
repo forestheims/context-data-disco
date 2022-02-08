8a2b2f4e452ade737627f84a0de01b25e73d5b43
forestheims <forestheims@gmail.com>
2022-02-08 15:46:17 -0800

    footer content added and styled.

--- src/components/composition/Footer.jsx
+++ src/components/composition/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import logo from '../../assets/GitHub_Logo.png';
 
 export default function Footer() {
-  return <footer>Footer Presentational</footer>;
+  return (
+    <footer>
+      <a href="https://github.com/forestheims">
+        <img alt="GitHub link" src={logo} style={{ width: '55px' }} />
+      </a>
+    </footer>
+  );
 }
