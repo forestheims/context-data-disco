a7b4dc62cff94f25c03f28c0584fbb8eefa110c0
forestheims <forestheims@gmail.com>
2022-02-07 18:12:55 -0800

    setting up entries map on EntryList.

--- src/components/EntryForm/EntryForm.jsx
+++ src/components/EntryForm/EntryForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useAppContext } from '../../context/Composition';
 
 export default function EntryForm() {
+  const { user } = useAppContext();
   return (
     <>
       <h2>EntryForm</h2>
--- src/components/composition/EntryList/EntryList.jsx
+++ src/components/composition/EntryList/EntryList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useAppContext } from '../../../context/Composition';
 
 export default function EntryList() {
+  const { user, entries } = useAppContext();
   return (
     <>
       <h2>EntryList</h2>
+      {entries.map(() => {})}
     </>
   );
 }
