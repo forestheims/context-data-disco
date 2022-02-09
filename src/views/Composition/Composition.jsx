94d86faf80d9b0167ca011ecd805d95a41312b92
forestheims <forestheims@gmail.com>
2022-02-08 18:46:39 -0800

    background color now remains, even when list of entries goes beyond view height.

--- src/views/Composition/Composition.jsx
+++ src/views/Composition/Composition.jsx
@@ -4,10 +4,12 @@ import Header from '../../components/composition/Header';
 
 export function Composition({ children }) {
   return (
-    <div className="composition">
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <div className="composition">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
   );
 }
