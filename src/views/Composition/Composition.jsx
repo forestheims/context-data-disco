22583506e2788ab015ec2750ae6a766530b1a70a
forestheims <forestheims@gmail.com>
2022-02-09 12:21:17 -0800

    more component tests added.

--- src/views/Composition/Composition.jsx
+++ src/views/Composition/Composition.jsx
@@ -4,12 +4,10 @@ import Header from '../../components/composition/Header';
 
 export function Composition({ children }) {
   return (
-    <>
-      <div className="composition">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
+    <div className="composition">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
   );
 }
